ef3fc2c779536eec55dd17da3234ad16cf91ceef
zhengjiezxxy <zhengjiezxxy2010@gmail.com>
2014-04-10 21:25:45 +0800

    deploy to heroku

--- features/step_definitions/signup.rb
+++ features/step_definitions/signup.rb
@@ -1,12 +1,13 @@
 Given /^I am a guest$/ do
 end
 
-Then /^I can sign up$/ do
+Then /^I can register as a reader$/ do
   visit new_user_registration_path
   fill_in 'Email', :with=>'xxx@gmail.com'
   fill_in 'Password', :with=>'12345678'
   fill_in 'Password confirmation', :with=>'12345678'
   click_on 'Sign Up'
   page.should have_content("You have successfully registed up!")
+  page.should_not have_content("Edit")
 end
 
